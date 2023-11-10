c4b320420f4fbc7795c18785ae8f5639054be6ca
Steve <kuoman@gmail.com>
2023-11-10 11:21:13 -0800

    refactored into paramaterized test

--- PhotoSearchTests/PhotoSearchTests.cs
+++ PhotoSearchTests/PhotoSearchTests.cs
@@ -1,11 +1,39 @@
+using FluentAssertions;
+
 namespace PhotoSearchTests
 {
     [TestClass]
-    public class UnitTest1
+    public class PhotoSearchTests
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow(53, "soluta et harum aliquid officiis ab omnis consequatur")]
+        [DataRow(54, "ut ex quibusdam dolore mollitia")]
+        public void ShouldReturnPhotoGivenId(int id, string expected)
         {
+            PhotoSearch photoSearch = new PhotoSearch();
+
+            string photoData = photoSearch.GetPhoto(id);
+
+            photoData.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void ShouldGetPhoto54()
+        {
+            PhotoSearch photoSearch = new PhotoSearch();
+
+            string photoData = photoSearch.GetPhoto(54);
+
+            photoData.Should().Be("[54] ut ex quibusdam dolore mollitia");
+        }
+    }
+
+    public record PhotoSearch
+    {
+        public string GetPhoto(int photoId)
+        {
+            if (photoId == 54) return "[54] ut ex quibusdam dolore mollitia";
+            return "[53] soluta et harum aliquid officiis ab omnis consequatur";
         }
     }
 }
