9d7dc1ab80a65743ecbf5cf017d839a9693f68ce
Steve <kuoman@gmail.com>
2023-11-10 13:14:37 -0800

    udpate .gitignore and photos

--- PhotoSearchTests/PhotoDataTests.cs
+++ PhotoSearchTests/PhotoDataTests.cs
@@ -14,5 +14,26 @@ namespace PhotoSearchTests
             photoData.NumberOfPhotos().Should().Be(5000);
         }
 
+        [TestMethod]
+        public void ShouldContain100Albums()
+        {
+            PhotoData photoData = new PhotoData();
+
+            photoData.NumberOfAlbums().Should().Be(100);
+        }
+
+        [TestMethod]
+        [DataRow(53, "[53] soluta et harum aliquid officiis ab omnis consequatur")]
+        [DataRow(54, "[54] ut ex quibusdam dolore mollitia")]
+        public void ShouldReturnPhotoGivenId(int id, string expected)
+        {
+            PhotoData photoData = new PhotoData();
+
+            Photo photo = photoData.GetPhotoById(id);
+
+            photo.id.Should().Be(id);
+            photo.url.Should().Be(expected);
+        }
+
     }
 }
