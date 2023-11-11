3c4885c913aceea6ea71da74617239915fe56063
Steve <kuoman@gmail.com>
2023-11-10 16:19:21 -0800

    Added exit command

--- PhotoSearchApp/Program.cs
+++ PhotoSearchApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Mime;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("To exit type (x)");
+
+string input = Console.ReadLine();
+
+if (input.ToLower()== "x") Environment.Exit(0);
