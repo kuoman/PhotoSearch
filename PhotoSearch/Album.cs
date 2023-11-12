25ae8e798ce0cd2c1fdf0babd14a238ef0e22b5e
Steve <kuoman@gmail.com>
2023-11-12 11:46:55 -0800

    moved Album out of tests

---
+++ PhotoSearch/Album.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoSearch;
+
+public record Album(int AlbumId, List<Photo> albumPhotos)
+{
+    public string ExportString()
+    {
+        if (albumPhotos == null) return $"Album {AlbumId} not found";
+        if (albumPhotos.Count == 0) return $"No photos in Album {AlbumId}";
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"photo-album {AlbumId}");
+
+        foreach (Photo photo in albumPhotos)
+        {
+            photo.ExportString(sb);
+        }
+
+        return sb.ToString();
+    }
+}
