ca4bbe87866edfd2907df569adf88b091cfa09f4
Steve <kuoman@gmail.com>
2023-11-12 11:17:53 -0800

    added albumId to ctor

--- PhotoSearchTests/AlbumTests.cs
+++ PhotoSearchTests/AlbumTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System.Text;
 
 namespace PhotoSearchTests
 {
@@ -8,19 +9,29 @@ namespace PhotoSearchTests
         [TestMethod]
         public void ShouldExportString()
         {
-            Album album = new Album();
+            Album album = new Album(3);
 
             string actual = album.ExportString();
 
             actual.Should().Contain("photo-album 3");
+            actual.Should().Contain("[53] soluta et harum aliquid officiis ab omnis consequatur");
+            actual.Should().Contain("[54] ut ex quibusdam dolore mollitia");
         }
     }
 
-    public class Album
+    public record Album(int AlbumId)
     {
         public string ExportString()
         {
-            return "photo-album 3";
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"photo-album {AlbumId}");
+            sb.Append(System.Environment.NewLine);
+            sb.Append("[53] soluta et harum aliquid officiis ab omnis consequatur");
+            sb.Append(System.Environment.NewLine);
+            sb.Append("[54] ut ex quibusdam dolore mollitia");
+            sb.Append(System.Environment.NewLine);
+
+            return sb.ToString();
         }
     }
 }
